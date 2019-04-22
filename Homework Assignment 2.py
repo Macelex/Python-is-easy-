e5f57de9ecc9ed5e45a0b7d9bb780ee60e3186a1
Maclex <43061178+Macelex@users.noreply.github.com>
2019-04-22 19:21:40 +0530

    Add files via upload

---
+++ Homework Assignment 2.py
@@ -0,0 +1,20 @@
+def Genre(text):
+	Genre = "EDM, Future Bass, Pop"
+	return Genre
+	
+Genre = Genre(Genre)
+print(Genre)
+
+
+def Artist(text):
+	Artist = "Martin Garrix, David Guetta, Jamie Scott & Romy Dya"
+	return Artist
+Artist = Artist(Artist)
+print(Artist)
+
+
+def Year(text):
+	Year = "2017"
+	return Year
+Year = Year(Year)
+print(Year)
